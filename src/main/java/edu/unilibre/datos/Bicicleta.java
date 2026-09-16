e6fea2545ee8758a348fe5d307a369fbd615d217
ASUS <daniela-mateusn@unilibre.edu.co>
2026-09-15 21:01:46 -0500

    Agragar datos

--- src/main/java/edu/unilibre/datos/Bicicleta.java
+++ src/main/java/edu/unilibre/datos/Bicicleta.java
@@ -1,5 +1,39 @@
 package edu.unilibre.datos;
 
-public class Bicicleta {
+import java.time.LocalDate;
 
+public class Bicicleta {
+    private int serial;
+    private String color;
+    private int documPropietario;
+    private LocalDate horaEntrada;
+
+    public int obtenerSerial() {
+        return serial;
+    }
+
+    public void modificarSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public String obtenerColor() {
+        return color;
+    }
+
+    public void modificarColor(String color) {
+        this.color = color;
+    }
+
+    public int obtenerDocPropietario() {
+        return documPropietario;
+    }
+    public void modificarDocPropietario(int docPropietario) {
+        this.documPropietario = docPropietario;
+    }
+    public LocalDate obtenerHoraEntrada() {
+        return horaEntrada;
+    }
+    public void modificarHoraEntrada(LocalDate horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
 }
---
+++ src/main/java/edu/unilibre/datos/TipoPago.java
@@ -0,0 +1,6 @@
+package edu.unilibre.datos;
+
+public enum TipoPago {
+    EFECTIVO,
+    TARGETA;
+}
